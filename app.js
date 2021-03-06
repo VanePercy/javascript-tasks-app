d2382ca7161340deb2b5371e21578187a50f1e54
VanePercy <vanerodriguezpercy@gmail.com>
2021-03-06 13:35:22 -0500

    Logica de la aplicacion con JS

---
+++ app.js
@@ -0,0 +1,8 @@
+document.getElementById("formTask").addEventListener("submit", saveTask);
+
+function saveTask(e) {
+   
+  let title = document.getElementById("title").value;
+  let description = document.getElementById("description").value;
+  console.log(title, description);
+}
