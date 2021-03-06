8c98defb27b96b07c989eeb3a100b8bd6c2ac9a4
VanePercy <vanerodriguezpercy@gmail.com>
2021-03-06 16:40:39 -0500

    Guardar, listar en la App

--- app.js
+++ app.js
@@ -1,8 +1,52 @@
+// Action button
 document.getElementById("formTask").addEventListener("submit", saveTask);
 
+// Get the values with getElementById
 function saveTask(e) {
-   
   let title = document.getElementById("title").value;
   let description = document.getElementById("description").value;
   console.log(title, description);
+
+  // Store the values in object
+  let task = {
+    title: title,
+    description: description,
+  };
+
+  // Local storage
+  if (localStorage.getItem("tasks") == null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  getTasks();
+  e.preventDefault();
 }
+
+function getTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  let tasksView = document.getElementById("tasks");
+
+  tasksView.innerHTML = "";
+
+  for (let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+
+    tasksView.innerHTML += `<div class="card mb-2">
+    <div class="card-body">
+        <p>${title} - ${description}</p>
+        <a class="btn btn-danger">
+        Delete
+        </a>
+    </div>
+    </div>`
+  }
+}
+
+getTasks();
